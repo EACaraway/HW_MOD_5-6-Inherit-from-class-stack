3760f7edefa849ad57c894c817c594f9ba3149e1
Evan Caraway <evan.caraway@outlook.com>
2020-11-25 10:34:01 -0800

    Project Files

---
+++ HW MOD 5 and 6_4 last element in stack/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_MOD_5_and_6_4_last_element_in_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack myStack = new MyStack();
+            myStack.Push(20);
+            myStack.Push("Bacon");
+            myStack.Push(5);
+            myStack.Push("Eggs");
+            myStack.Push("Smile");
+            myStack.Push("Need Sleep");
+            myStack.LastElement();
+        }
+    }
+
+    public class MyStack: Stack
+    {
+        /// <summary>
+        /// Displays the last element in MyStack.
+        /// </summary>        
+        public void LastElement()
+        {
+            Console.WriteLine($"The last element in the Stack is: {Peek()}.");
+        }
+    }
+}
